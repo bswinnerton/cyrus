1ee1133d04708e3a9f0155a76dcd2c87051b20f4
Brooks Swinerton <bswinnerton@gmail.com>
2013-07-22 20:10:21 -0400

    Removing debugging tools

--- Gemfile
+++ Gemfile
@@ -3,7 +3,3 @@ ruby '2.0.0'
 
 gem 'rspec'
 gem 'rake'
-
-# Debugging
-gem 'pry'
-gem 'awesome_print'
--- lib/input_data.rb
+++ lib/input_data.rb
@@ -1,6 +1,4 @@
 require 'csv'
-require 'pry'
-require 'awesome_print'
 
 class InputData
   attr_accessor :parsed, :delimiter, :raw_data
